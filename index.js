7bc92b238deecc9912fc13197757e7d2c8dfe4fd
Grant Register <register.grant@gmail.com>
2022-08-02 15:33:50 -0600

    added a video and produced an index.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 //Objects
+// How come Employee's value is declared but never read?
 const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
